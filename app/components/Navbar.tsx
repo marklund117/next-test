1c476738926cc34db034723736f6d8f8654879e2
Mark Lund <marklund117@gmail.com>
2024-11-14 23:29:06 -0700

    we're getting started - next up is a footer component, and media breakpoints before we go too far

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,15 +10,16 @@ export default function Navbar () {
     return (
         // we will use a normal nav element with an unordered list
         // note the lack of <a> tags since we are using the Link component
-        <nav>
-            <ul>
+        <nav className="flex p-4 h-[56px] shadow-sm">
+            <span className="text-xl font-bold mr-4">Adviser Elite</span>
+            <ul className="list-none flex gap-4">
                 <li>
-                    <Link href="/">
+                    <Link className="transition hover:transiton-all duration-300 hover:bg-[#0098f1] text-slate-100 rounded-lg bg-[#0078be] px-4 py-2" href="/">
                         Link 1
                     </Link>
                 </li>
                 <li>
-                    <Link href="/">
+                    <Link className="transition hover:transiton-all duration-300 hover:bg-[#0098f1] text-slate-100 rounded-lg bg-[#0078be] px-4 py-2" href="/">
                         Link 2
                     </Link>
                 </li>
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@
 // first, import React
 import React from 'react'
 
+// import tailwind global CSS
+import './styles/globals.css'
+
+// here's the main dish
 export default function Page() {
-  return <div>Test text.</div>
+  return <div className="p-4">Main Content.</div>
 }
