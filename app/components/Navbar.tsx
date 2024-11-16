ef1bbd7c8a5e1d70f22172738ba1435e6dc985ce
Mark Lund <marklund117@gmail.com>
2024-11-15 20:53:14 -0700

    menus can be a challenge

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,33 @@
+
 // Navbar component file
+// useState requires us to define this as a client component
+'use client'
 // first, import React
 import React from 'react'
+// we also need useState
+import { useState } from 'react'
 
 // next, import the link component from next (hmmm)
 import Link from 'next/link'
 
 // create a component named Navbar
 export default function Navbar () {
+
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         // we will use a normal nav element with an unordered list
         // note the lack of <a> tags since we are using the Link component
         <nav className="flex p-4 h-[56px] shadow-sm">
             <span className="text-xl font-bold mr-4">Adviser Elite</span>
-            <ul className="list-none flex gap-4">
+            <div className="md:hidden">
+                <button onClick={() => setIsOpen(!isOpen)} className="">
+                    menu button
+                </button>
+            </div>
+            <ul className={`list-none flex md:flex-row gap-4 justify-end
+                ${isOpen ? 'flex-col md:flex-row' : 'hidden md:flex-row'}`
+            }>
                 <li>
                     <Link className="transition hover:transiton-all duration-300 hover:bg-[#0098f1] text-slate-100 rounded-lg bg-[#0078be] px-4 py-2" href="/">
                         Link 1
@@ -23,8 +38,12 @@ export default function Navbar () {
                         Link 2
                     </Link>
                 </li>
+                <li>
+                    <Link className="transition hover:transiton-all duration-300 hover:bg-[#0098f1] text-slate-100 rounded-lg bg-[#0078be] px-4 py-2" href="/">
+                        Link 3
+                    </Link>
+                </li>
             </ul>
         </nav>
     )
 }
-
