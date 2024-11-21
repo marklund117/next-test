f3e6aa4170dff030999bf35e0fe9a7ebfc2b65fa
Mark Lund <marklund117@gmail.com>
2024-11-21 00:24:27 -0700

    added logo image to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,9 +2,13 @@
 // first, import react
 import React from 'react'
 
+// we also need to import the image
+import whitelogo from '../images/whitelogo.png'
+
 export default function Footer() {
     return (
         <footer className="flex flex-col items-center p-4 bg-[#111111] text-[#EEEEEE]">
+            <img className="h-[32px] mb-6" src={whitelogo.src}/>
             <p className="mb-3">Contact us via email at <a className="underline" href="mailto:info@adviserelite.com">info@adviserelite.com</a></p>
             <p className="mb-3">Follow us on <a className="underline" target="_blank" href="https://www.linkedin.com/company/adviserelite/">Linkedin</a></p>
             <p className="mb-3">Copyright &copy;2024 Adviser Elite, LLC. All rights reserved.</p>
